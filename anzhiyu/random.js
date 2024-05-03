c664a88a68cdc904558e8af30c274a8a165085d2
24266 <1994589823@qq.com>
2024-05-03 20:08:40 +0800

    Site updated: 2024-05-03 20:08:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/02/hello-world/"];function toRandomPost(){
+var posts=["undefined/16107.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
